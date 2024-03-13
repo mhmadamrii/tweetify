70b48ffe0d3502c6df64919c740b5bda0f126485
mhmadamrii <interceptorghost4@gmail.com>
2024-03-14 05:38:50 +0700

    fix: error pre-rendering due invalid useSearchParams hooks

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,9 +1,14 @@
 import Authenticated from '~/components/shared/authenticated';
+import { Suspense } from 'react';
 
 export default function Layout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <Authenticated>{children}</Authenticated>;
+  return (
+    <Suspense fallback={<h1>Loading the Stream....</h1>}>
+      <Authenticated>{children}</Authenticated>;
+    </Suspense>
+  );
 }
--- src/components/shared/client-only.tsx
+++
@@ -1,7 +0,0 @@
-'use client';
-
-export default function ClientOnly({
-  children,
-}: Readonly<{ children: React.ReactNode }>) {
-  return <section>{children}</section>;
-}
