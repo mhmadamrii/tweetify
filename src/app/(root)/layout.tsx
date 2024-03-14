4df68326cac5287cc70898cc3ad2663ccc51a6b0
mhmadamrii <interceptorghost4@gmail.com>
2024-03-14 17:36:10 +0700

    feat: activate

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,14 +1,14 @@
 import Authenticated from '~/components/shared/authenticated';
 import { Suspense } from 'react';
 
-export default function Layout({
+export default async function Layout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
     <Suspense fallback={<h1>Loading the Stream....</h1>}>
-      <Authenticated>{children}</Authenticated>;
+      <Authenticated>{children}</Authenticated>
     </Suspense>
   );
 }
