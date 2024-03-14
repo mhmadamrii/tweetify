a1783b249cd45601c1ada71f857d01e277b132f0
mhmadamrii <interceptorghost4@gmail.com>
2024-03-14 19:55:06 +0700

    fdlak;

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 import Authenticated from '~/components/shared/authenticated';
 import { Toaster } from '~/components/ui/toaster';
+import { NextAuthProvider } from './providers';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -18,7 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Toaster />
-        {children}
+        <NextAuthProvider>{children}</NextAuthProvider>
       </body>
     </html>
   );
